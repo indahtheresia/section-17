4e5957e585fef9c969be731ed1e75ab292361180
Indah Theresia <indahtheresiatheh29@gmail.com>
2024-12-31 10:36:01 +0800

    Build & use a reusable input component

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Input from './Input.jsx';
 
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState('');
@@ -14,6 +15,7 @@ export default function Login() {
   });
 
   const emailIsInvalid = didEdit.email && !enteredValue.email.includes('@');
+  const passwordIsInvalid = didEdit.password && enteredValue.password.trim().length < 6;
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -51,18 +53,9 @@ export default function Login() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value= {enteredValue.email} onBlur={() => handleInputBlur('email')} />
-          <div className= 'control-error'>
-            {emailIsInvalid && <p>Please enter a valid email address.</p>}
-          </div>
-        </div>
-
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange= {(event) => handleInputChange('password', event.target.value)} value={enteredValue.password} />
-        </div>
+        <Input label="Email" id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value= {enteredValue.email} onBlur={() => handleInputBlur('email')} error={emailIsInvalid && 'Please enter a valid email!'} />
+        
+        <Input label="Password" id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value= {enteredValue.password} onBlur={() => handleInputBlur('password')} error={passwordIsInvalid && 'Please enter a valid password!'} />
       </div>
 
       <p className="form-actions">
